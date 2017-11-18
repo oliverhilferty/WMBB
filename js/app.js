87f626090d98e2846df00c7650e93e8a69c02047
Oliver Hilferty <oliver.hilferty@rezonence.com>
2017-11-18 13:37:14 +0000

    Setup refactored functions

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
-// alert("Start");
-$("document").ready(function() {
+var debug = true;
 
-  $("input").focus(function() {
-  	$("label").css("color", "#e42e2e");
-  });
-  $("input").blur(function() {
-  	$("label").css("color", "#999999");
-  });
+$(document).ready(function() {
 
-  $("div.submit").click(function() {
-    clearTable();
-    var smsCode = $('input').val();
-    console.log(smsCode);
-    getRoutes(smsCode);
-  });
+  // Event Listeners
+  $('.submitButton').click(function() {
 
+    // Get SMS code from input field
+    var smsCode = $('.smsCode').val();
+    log(smsCode);
+    getArrivals(smsCode).then(function(response) {
+      log(response);
+    });
+  });
 });
 
-function clearTable() {
-  $("td").remove();
-}
 
+/**
+ * isDST() determines whether we are currently in daylight saving time
+ * @returns {boolean} true or false, where true
+ * indicates being in daylight saving time
+ */
 function isDST() {
   Date.prototype.stdTimezoneOffset = function() {
     var jan = new Date(this.getFullYear(), 0, 1);
@@ -36,130 +35,39 @@ function isDST() {
   return today.dst();
 }
 
-function generateRow(route, towards, time) {
-	var tr = document.createElement("tr");
-
-  var td1 = document.createElement("td");
-  td1.innerHTML = route;
-
-  var td2 = document.createElement("td");
-  td2.innerHTML = towards;
-
-  var td3 = document.createElement("td");
-  td3.innerHTML = time;
-
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-
-  $('table')[0].appendChild(tr);
+/**
+ * clearTable() clears all rows from the table
+ */
+function clearTable() {
+  $('td').remove();
 }
 
-function generateDirection(routes) {
-  console.log("start");
-  $(".arrivalsWrapper1").css("display", "none");
-  $(".arrivalsWrapper2").css("display", "none");
+/**
+ * getArrivals() gets the arrival times for all buses at a specific bus stop
+ * @param {String} smsCode The Naptan ID of a particular bus stop
+ * @returns {Object} An object containing bus arrival times for the given smsCode
+ */
+async function getArrivals(smsCode) {
 
-  if (routes.length == 2) { //two directions
-    $(".arrivalsWrapper1").css("display", "block");
+  // Search for the stop code
+  var data = await $.getJSON(`https://api.tfl.gov.uk/StopPoint/Search/${smsCode}`);
 
-    var routes1 = routes[0];
-    var routes2 = routes[1];
+  // Extract the stop id from the search results
+  var id = data.matches[0].id;
 
-    $('.arrivalsTop').html(routes1.split('-')[0]);
-    $('.arrivalsBottom').html(routes2.split('-')[0]);
+  // Get the arrivals data
+  var arrivals = await $.getJSON(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`);
 
-    $(".arrivalsTop").click(function() {
-      getArrivals(routes1.split("-")[1]);
-    });
-    $(".arrivalsBottom").click(function() {
-      getArrivals(routes2.split("-")[1]);
-    });
-  } else { // one direction
-    $(".arrivalsWrapper2").css("display", "block");
+  return arrivals;
+}
 
-    $('.arrivalsSingle').html(routes[0].split('-')[0]);
-
-    $(".arrivalsSingle").click(function() {
-      getArrivals(routes[0].split("-")[1]);
-    });
+/**
+ * Log function for debugging
+ * @param {String} message - The message to log to the screen and console
+ */
+function log(message) {
+  if (debug == true) {
+    console.log(message);
+    Materialize.toast(message, 4000);
   }
 }
-
-
-function getTimeUntil(hours, minutes, seconds) {
-  var time = new Date();
-  time.setHours(hours);
-  time.setMinutes(minutes);
-  time.setSeconds(seconds);
-  time.setMilliseconds( 0 );
-  var timeUntil = (time.getTime() - new Date().getTime()) / 1000 / 60;
-  timeUntil = Math.round(timeUntil);
-
-  if (timeUntil <= 0) {timeUntil = 0;}
-
-  return timeUntil;
-}
-
-function getArrivals(naptanId) {
-  buses = [];
-  return $.getJSON('https://api.tfl.gov.uk/StopPoint/' + naptanId + '/arrivals', function(data) {
-    for (var i = 0; i < data.length; i++) {
-      var busData = data[i];
-      var bus = {};
-
-      bus.id = busData.lineId;
-      bus.destination = busData.destinationName;
-
-      var busDataTime = busData.expectedArrival.split("T")[1].replace("Z", "").split(":");
-      var busDataHour = parseInt(busDataTime[0]) + (isDST() ? 1 : 0);
-      var busDataMin = parseInt(busDataTime[1]);
-      var busDataSec = parseInt(busDataTime[2]);
-      var timeUntil = getTimeUntil(busDataHour, busDataMin, busDataSec);
-      timeUntil = Math.round(timeUntil);
-      if (timeUntil < 0) {timeUntil = 0;}
-
-      bus.time = timeUntil;
-
-      buses.push(bus);
-    }
-
-    buses.sort(function(a, b) {
-      return parseFloat(a.time) - parseFloat(b.time);
-    });
-
-    for (var i = 0; i < buses.length; i++) {
-      var bus = buses[i];
-      var time = bus.time;
-      if (time == 0) {
-        time = "Due";
-      } else if (time == 1) {
-        time = "1 min";
-      } else {
-        time = time + " mins";
-      }
-
-      generateRow(bus.id, bus.destination, time);
-    }
-  });
-}
-
-function getRoutes(smsCode) {
-  $.getJSON("https://api.tfl.gov.uk/StopPoint/Sms/" + smsCode, function(data) {
-    var routes = [];
-    // alert(data.children.length)
-    for (var i = 0; i < data.children.length; i++) {
-      routes.push(data.children[i].additionalProperties[1].value
-        + "-" + data.children[i].naptanId);
-    }
-    console.log(routes);
-    //return routes;
-    console.log("before function call");
-    generateDirection(routes);
-  });
-}
-
-// NAPTAN ID : 490011217S
-
-// getArrivals("490011217S");
-// $(".arrivalsWrapper1").css("display", "block");
