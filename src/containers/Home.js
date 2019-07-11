db72262fbf50cb223b3c0425734b44de1c0bf39d
oliverhilferty <oliver.hilferty@rezonence.com>
2019-07-11 14:41:23 +0100

    Add home container

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import 'jquery';
+import 'materialize-css/dist/js/materialize.js';
+import 'materialize-css/dist/css/materialize.css';
+import "./Home.css";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+
+            </div>
+        );
+    }
+}
