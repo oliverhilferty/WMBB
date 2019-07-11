083fbbe3c13005e110c5d316938845d96c85581a
oliverhilferty <oliver.hilferty@rezonence.com>
2019-07-11 14:09:57 +0100

    Recreate navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import 'jquery';
 import 'materialize-css/dist/js/materialize.js';
 import 'materialize-css/dist/css/materialize.css';
 import './App.css';
-import { Container} from 'react-materialize';
+import { Container } from 'react-materialize';
 
 class App extends Component {
     render() {
         return (
-            <Container>
+            <div className="App app-container">
+                <nav>
+                    <div className="nav-wrapper red darken-3">
+                        <Container>
+                            <a href="/" className="wmbb breadcrumb">Where's My Bloody Bus!?</a>
+                        </Container>
+                    </div>
+                </nav>
+                <Container>
 
-            </Container>
+                </Container>
+            </div>
         );
     }
 }
