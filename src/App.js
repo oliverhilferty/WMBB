c866e19e04c8d32f566fef2f9df97858333b4f8a
oliverhilferty <oliver.hilferty@rezonence.com>
2019-07-11 13:49:11 +0100

    Update App.js with materialize imports and container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'jquery';
+import 'materialize-css/dist/js/materialize.js';
+import 'materialize-css/dist/css/materialize.css';
 import './App.css';
+import { Container} from 'react-materialize';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Container>
+
+            </Container>
+        );
+    }
 }
 
 export default App;
