319efa2a789929897121a2e034b31554a60bc892
oliverhilferty <oliverhilferty@gmail.com>
2019-07-11 15:42:56 +0100

    Add text field and button to Home container

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,13 +3,17 @@ import 'jquery';
 import 'materialize-css/dist/js/materialize.js';
 import 'materialize-css/dist/css/materialize.css';
 import './Home.scss';
-import {TextInput} from 'react-materialize';
+import {TextInput, Button} from 'react-materialize';
 
 export default class Home extends Component {
     render() {
         return (
             <div className="Home">
-
+                <TextInput label="Bus Stop Code" className="noHelper"/>
+                <Button
+                    waves="light"
+                    className="red darken-3"
+                >Submit</Button>
             </div>
         );
     }
