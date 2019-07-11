55cc6421b6601a51a2533d00fb7e56a555b0acf7
oliverhilferty <oliverhilferty@gmail.com>
2019-07-11 18:28:58 +0100

    Create function to calculate time until a given time

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -2,7 +2,7 @@
  * isDST() determines whether we are currently in daylight saving time
  * @returns {boolean} where true indicates being in daylight saving time
  */
-function isDST() {
+export const isDST = () => {
     Date.prototype.stdTimezoneOffset = function() {
         const jan = new Date(this.getFullYear(), 0, 1);
         const jul = new Date(this.getFullYear(), 6, 1);
@@ -15,4 +15,17 @@ function isDST() {
 
     const today = new Date();
     return today.dst();
+};
+
+/**
+ * Gets the time until a given datetime
+ * @param {String} time - A datetime string
+ * @returns {Number} The time until the given time, rounded to the nearest minute
+ */
+function timeUntil(time) {
+    const then = new Date(time);
+    const now = new Date();
+    const difference = then - now;
+    const mins = difference / 1000 / 60;
+    return Math.round(mins);
 }
