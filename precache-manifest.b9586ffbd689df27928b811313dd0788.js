54206dce6f6136e90a608cfc1d4190451a193dd8
oliverhilferty <oliverhilferty@gmail.com>
2019-07-12 16:40:08 +0100

    Updates

---
+++ precache-manifest.b9586ffbd689df27928b811313dd0788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2e02c2d3742be26443bb6536f86a5e",
+    "url": "/WMBB/index.html"
+  },
+  {
+    "revision": "2484d850c7b727d5efa9",
+    "url": "/WMBB/static/css/2.a8967f2d.chunk.css"
+  },
+  {
+    "revision": "8c3434e7e14b824d044f",
+    "url": "/WMBB/static/css/main.56ce23ed.chunk.css"
+  },
+  {
+    "revision": "2484d850c7b727d5efa9",
+    "url": "/WMBB/static/js/2.9fd27684.chunk.js"
+  },
+  {
+    "revision": "8c3434e7e14b824d044f",
+    "url": "/WMBB/static/js/main.d1680c51.chunk.js"
+  },
+  {
+    "revision": "460299734bde5e4724d2",
+    "url": "/WMBB/static/js/runtime~main.9e5386ca.js"
+  },
+  {
+    "revision": "5136cbe62a63604402f2fedb97f246f8",
+    "url": "/WMBB/static/media/Roboto-Regular.5136cbe6.woff2"
+  }
+]);
