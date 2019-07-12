fedd3d06d3acb592e077682c6c7f635abe0dfe1c
oliverhilferty <oliverhilferty@gmail.com>
2019-07-12 09:56:45 +0100

    Add option to pass an instance of Date to the timeUntil function to use as 'now'

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -20,11 +20,11 @@ export const isDST = () => {
 /**
  * Gets the time until a given datetime
  * @param {String} time - A datetime string
+ * @param {Date} now = new Date() - Optionally, the time to use as the current time
  * @returns {Number} The time until the given time, rounded to the nearest minute
  */
-function timeUntil(time) {
+function timeUntil(time, now = new Date()) {
     const then = new Date(time);
-    const now = new Date();
     const difference = then - now;
     const mins = difference / 1000 / 60;
     return Math.round(mins);
