d774f86a57ce576599b403c4ff0bbbd8269c9155
oliverhilferty <oliver.hilferty@rezonence.com>
2019-07-18 15:21:23 +0100

    Update navbar with links to home and user stops page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import 'jquery';
 import 'materialize-css/dist/js/materialize.js';
 import 'materialize-css/dist/css/materialize.css';
 import './App.scss';
-import { Container } from 'react-materialize';
+import { Container, Navbar, NavItem } from 'react-materialize';
+import {Link} from "react-router-dom";
 import Routes from './Routes';
 
 class App extends Component {
     render() {
         return (
             <div className="App app-container">
-                <nav>
-                    <div className="nav-wrapper red darken-3">
-                        <Container>
-                            <a href="/" className="breadcrumb">Where's My Bloody Bus!?</a>
-                        </Container>
-                    </div>
-                </nav>
+                <Navbar
+                    className="red darken-3"
+                    alignLinks="right"
+                    brand={
+                        <Link to="/">Where's My Bloody Bus!?</Link>
+                    }
+                    sidenav={<>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/user-stops">My Stops</Link></li>
+                    </>}
+                >
+                    <NavItem href="/">
+                        <Link to="/">Home</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/user-stops">My Stops</Link>
+                    </NavItem>
+                </Navbar>
                 <Container>
                     <Routes />
                 </Container>
--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import UserStops from "./containers/UserStops";
 
 export default () =>
     <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/user-stops" exact component={UserStops}/>
     </Switch>;
